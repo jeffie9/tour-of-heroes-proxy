580630562bcaf22769e7aa36f6e1d6ffe00394d6
Jeff Eltgroth <jeffie89@gmail.com>
2018-01-28 15:27:22 -0600

    still simple mock api, but now with typescript

---
+++ src/mock-api/app.ts
@@ -0,0 +1,62 @@
+import * as path from 'path';
+import * as express from 'express';
+import * as logger from 'morgan';
+import * as bodyParser from 'body-parser';
+
+const heroes = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
+// Creates and configures an ExpressJS web server.
+class App {
+
+  // ref to Express instance
+  public express: express.Application;
+
+  //Run configuration methods on the Express instance.
+  constructor() {
+    this.express = express();
+    this.middleware();
+    this.routes();
+  }
+
+  // Configure Express middleware.
+  private middleware(): void {
+    this.express.use(logger('dev'));
+    this.express.use(bodyParser.json());
+    this.express.use(bodyParser.urlencoded({ extended: false }));
+  }
+
+  // Configure API endpoints.
+  private routes(): void {
+    /* This is just to get up and running, and to make sure what we've got is
+     * working so far. This function will change when we start to add more
+     * API endpoints */
+    let router = express.Router();
+    // placeholder route handler
+    router.get('/', (req, res, next) => {
+      res.json({
+        message: 'Hello World!'
+      });
+    });
+
+    router.get('/api/heroes', function(req, res, next){
+		res.json(heroes);
+	});
+
+    
+    this.express.use('/', router);
+  }
+
+}
+
+export default new App().express;
